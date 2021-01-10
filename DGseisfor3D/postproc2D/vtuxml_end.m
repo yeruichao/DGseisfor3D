74a644c7a86992bab0d582a19b2f7f3b8c74f7e8
Ruichao Ye <richao.ye@gmail.com>
2021-01-10 16:03:12 +0000

    deleting old version

--- DGseisfor3D/postproc2D/vtuxml_end.m
+++
@@ -1,9 +0,0 @@
-function vtuxml_end(fid)
-
-fwrite(fid,char(10),'char*1');
-fprintf(fid,'  </AppendedData>\n');
-fprintf(fid,'</VTKFile>');
-fclose(fid);
-
-return
-end
---
+++ readme.md
@@ -0,0 +1,2 @@
+# DGseisfor3D
+Forward simulation software for 3D acousto-elastic wave and dynamic rutpure simulation based on Discontinuous Galerkin method
