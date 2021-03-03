16806aa186944159846a9285098213e581408fc5
Ruichao Ye <richao.ye@gmail.com>
2021-03-03 15:20:31 +0000

    reorg directory

--- demo/planar_rupture/planar_rupt_recv.py
+++ demo/planar_rupture/planar_rupt_recv.py
@@ -1,9 +1,9 @@
 import numpy
 import sys
-sys.path.insert(0,'../../')
-import meshtools.draw_util as draw_util
-import meshtools.trimesh as trimesh
-import meshtools.mesh_util as mesh_util
+sys.path.insert(0,'../../meshtools')
+import draw_util as draw_util
+import trimesh as trimesh
+import mesh_util as mesh_util
 
 recvx=numpy.arange(-4.0,5.0,2.0)
 recvy=recvx-6.0
